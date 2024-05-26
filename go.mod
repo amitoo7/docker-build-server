30873ddd263301260ab9476e45e54eb68c7508f6
amitoo7 <>
2024-05-26 14:23:39 +0530

    added feature deploy but commit id is not getting need to find some other way to handle it

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/net v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 var upgrader = websocket.Upgrader{
@@ -25,6 +25,14 @@ var upgrader = websocket.Upgrader{
 
 var clients = make(map[string]*websocket.Conn)
 var mu sync.Mutex
+
+type Project struct {
+	ID         int    `json:"id"`
+	RepoURL    string `json:"repoUrl"`
+	Token      string `json:"token"`
+	Autodeploy bool   `json:"autodeploy"`
+	Branch     string `json:"branch"`
+}
 
 type BuildRequest struct {
 	RepoUrl string `json:"repoUrl"`
@@ -52,7 +60,8 @@ var db *sql.DB
 
 func initDB() {
 	var err error
-	db, err = sql.Open("sqlite3", "./builds.db")
+	connStr := "postgres://postgres:postgres@localhost/mydb?sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -61,26 +70,51 @@ func initDB() {
         id TEXT PRIMARY KEY,
         repo_url TEXT,
         commit_id TEXT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     `
 	_, err = db.Exec(createTable)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	createProjectTable := `
+    CREATE TABLE IF NOT EXISTS projects (
+        id SERIAL PRIMARY KEY,
+        repo_url TEXT,
+        token TEXT,
+        autodeploy BOOLEAN,
+        branch TEXT
+    );
+    `
+	_, err = db.Exec(createProjectTable)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func saveBuild(buildID, repoURL, commitID string) error {
-	_, err := db.Exec("INSERT INTO builds (id, repo_url, commit_id) VALUES (?, ?, ?)", buildID, repoURL, commitID)
+	_, err := db.Exec("INSERT INTO builds (id, repo_url, commit_id) VALUES ($1, $2, $3)", buildID, repoURL, commitID)
 	return err
 }
 
 func getLastBuild() (BuildResponse, error) {
-
 	var build BuildResponse
 	row := db.QueryRow("SELECT id, commit_id FROM builds ORDER BY timestamp DESC LIMIT 1")
 	err := row.Scan(&build.BuildId, &build.CommitID)
 	return build, err
+}
+
+func saveProject(repoURL, token string, autodeploy bool, branch string) error {
+	_, err := db.Exec("INSERT INTO projects (repo_url, token, autodeploy, branch) VALUES ($1, $2, $3, $4)", repoURL, token, autodeploy, branch)
+	return err
+}
+
+func getCurrentProject() (Project, error) {
+	var project Project
+	row := db.QueryRow("SELECT id, repo_url, token, autodeploy, branch FROM projects ORDER BY id DESC ")
+	err := row.Scan(&project.ID, &project.RepoURL, &project.Token, &project.Autodeploy, &project.Branch)
+	return project, err
 }
 
 func buildHandler(w http.ResponseWriter, r *http.Request) {
@@ -89,11 +123,10 @@ func buildHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&req)
 
 	buildId := uuid.New().String()
-
 	var commitID string
 
+	// Start the build process in a separate goroutine
 	go func() {
-		// Clone the repository
 		repoDir := fmt.Sprintf("/tmp/%s", buildId)
 		cmd := exec.Command("git", "clone", req.RepoUrl, repoDir)
 		cmd.Stdout = &LogStreamer{buildId: buildId}
@@ -122,15 +155,6 @@ func buildHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// Notify frontend that the build process is complete
-		mu.Lock()
-		if conn, ok := clients[buildId]; ok {
-			conn.WriteMessage(websocket.TextMessage, []byte("BUILD_COMPLETE"))
-			conn.Close()
-			delete(clients, buildId)
-		}
-		mu.Unlock()
-
 		// Save build details to the database
 		if err := saveBuild(buildId, req.RepoUrl, commitID); err != nil {
 			log.Printf("Error saving build details: %v", err)
@@ -140,10 +164,20 @@ func buildHandler(w http.ResponseWriter, r *http.Request) {
 		if err := os.RemoveAll(repoDir); err != nil {
 			log.Printf("Error removing repository directory: %v", err)
 		}
-	}()
 
-	resp := BuildResponse{BuildId: buildId, CommitID: commitID}
+		// Notify frontend that the build process is complete
+		mu.Lock()
+		if conn, ok := clients[buildId]; ok {
+			conn.WriteMessage(websocket.TextMessage, []byte("BUILD_COMPLETE"))
+			conn.Close()
+			delete(clients, buildId)
+		}
+		mu.Unlock()
+
+	}()
+	resp := BuildResponse{BuildId: buildId}
 	json.NewEncoder(w).Encode(resp)
+
 }
 
 func lastBuildHandler(w http.ResponseWriter, r *http.Request) {
@@ -154,6 +188,33 @@ func lastBuildHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	json.NewEncoder(w).Encode(build)
+}
+
+func projectHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	var req struct {
+		RepoURL    string `json:"repoUrl"`
+		Token      string `json:"token"`
+		Autodeploy bool   `json:"autodeploy"`
+		Branch     string `json:"branch,omitempty"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	err := saveProject(req.RepoURL, req.Token, req.Autodeploy, req.Branch)
+	if err != nil {
+		http.Error(w, "Could not save project details", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+func currentProjectHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	project, err := getCurrentProject()
+	if err != nil {
+		http.Error(w, "Could not get current project details", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(project)
 }
 
 func logsHandler(w http.ResponseWriter, r *http.Request) {
@@ -170,6 +231,30 @@ func logsHandler(w http.ResponseWriter, r *http.Request) {
 	clients[buildId] = conn
 	mu.Unlock()
 }
+
+func deployHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	var req struct {
+		RepoURL  string `json:"repoUrl"`
+		CommitID string `json:"commitId"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	// Use the commit ID as the image tag
+	imageName := fmt.Sprintf("myapp:%s", req.CommitID)
+	fmt.Println("image tag: ", imageName)
+	// Run the Docker container using the built image
+	cmd := exec.Command("docker", "run", "-d", "--name", "testcontainer", imageName)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("Error running Docker container: %v", err)
+		http.Error(w, "Error running Docker container", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("executing middleware", r.Method)
@@ -189,12 +274,16 @@ func CorsMiddleware(next http.Handler) http.Handler {
 func main() {
 	initDB()
 	defer db.Close()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/api/build", buildHandler).Methods("POST")
 	r.HandleFunc("/api/last-build", lastBuildHandler).Methods("GET")
+	r.HandleFunc("/api/project", projectHandler).Methods("POST")
+	r.HandleFunc("/api/current-project", currentProjectHandler).Methods("GET")
+	r.HandleFunc("/api/deploy", deployHandler).Methods("POST")
 	r.HandleFunc("/api/logs/{buildId}", logsHandler)
 
 	http.Handle("/", r)
-	fmt.Println("Running server on 8080 ")
+	fmt.Println("Running webserver on 8080: ")
 	log.Fatal(http.ListenAndServe(":8080", CorsMiddleware(r)))
 }
